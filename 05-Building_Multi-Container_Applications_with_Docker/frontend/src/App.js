e3ff2e077602842890747fbe9277d63a0f60742f
Nguyễn Quốc Anh Tuấn <147523268+tuan0919@users.noreply.github.com>
2024-07-26 17:55:34 +0700

    update: small fix

--- 05-Building_Multi-Container_Applications_with_Docker/frontend/src/App.js
+++ 05-Building_Multi-Container_Applications_with_Docker/frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
       setIsLoading(true);
 
       try {
-        const response = await fetch('http://localhost/goals');
+        const response = await fetch('http://goal-backend/goals');
 
         const resData = await response.json();
 
@@ -39,7 +39,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost/goals', {
+      const response = await fetch('http://goal-backend/goals', {
         method: 'POST',
         body: JSON.stringify({
           text: goalText,
@@ -78,7 +78,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost/goals/' + goalId, {
+      const response = await fetch('http://goal-backend/goals/' + goalId, {
         method: 'DELETE',
       });
 
--- 05-Building_Multi-Container_Applications_with_Docker/my-scripts.sh
+++ 05-Building_Multi-Container_Applications_with_Docker/my-scripts.sh
@@ -8,12 +8,12 @@ docker run -d --name mongodb --network goal-network -p 27017:27017 mongo
 cd 05-Building_Multi-Container_Applications_with_Docker\backend
 docker build -t goal-node:latest .
 # Run container backend
-docker run -p 80:80 -d --network goal-network --name goal-backend -v "C:\Users\Tuan\Desktop\my-docker-kubernates-learning\05-Building_Multi-Container_Applications_with_Docker\backend:/app" goal-node:latest
+docker run -p 80:80 --network goal-network --name goal-backend -v app/node_modules -v "C:\Users\Tuan\Desktop\my-docker-kubernates-learning\05-Building_Multi-Container_Applications_with_Docker\backend:/app" goal-node:latest
 docker logs goal-backend
 
 # Build Image cho front end
 cd 05-Building_Multi-Container_Applications_with_Docker\frontend
 docker build -t goal-react:latest .
 # Run container frontend
-docker run -p 3000:3000 --rm -d --network goal-network --name goal-frontend -v "C:\Users\Tuan\Desktop\my-docker-kubernates-learning\05-Building_Multi-Container_Applications_with_Docker\frontend:/app" goal-react:latest
+docker run -p 3000:3000 -it --network goal-network --name goal-frontend -v app/node_modules -v "C:\Users\Tuan\Desktop\my-docker-kubernates-learning\05-Building_Multi-Container_Applications_with_Docker\frontend:/app" goal-react:latest
 docker logs goal-frontend
