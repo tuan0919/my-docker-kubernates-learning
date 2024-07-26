fcb50b514ee2668f35b035b3ff34a08038f310ce
Nguyễn Quốc Anh Tuấn <147523268+tuan0919@users.noreply.github.com>
2024-07-27 00:17:21 +0700

    new: done making containers be able to communicate to each other

--- 05-Building_Multi-Container_Applications_with_Docker/my-scripts.sh
+++ 05-Building_Multi-Container_Applications_with_Docker/my-scripts.sh
@@ -2,18 +2,17 @@
 docker network create goal-network
 
 # Tải & chạy image cho mongodb, add image này vào goal-network
-docker run -d --name mongodb --network goal-network -p 27017:27017 mongo
+docker run --rm --name mongodb --network goal-network mongo
 
 # Build Image cho backend
 cd 05-Building_Multi-Container_Applications_with_Docker\backend
 docker build -t goal-node:latest .
 # Run container backend
 docker run -p 80:80 --network goal-network --name goal-backend -v app/node_modules -v "C:\Users\Tuan\Desktop\my-docker-kubernates-learning\05-Building_Multi-Container_Applications_with_Docker\backend:/app" goal-node:latest
-docker logs goal-backend
 
 # Build Image cho front end
 cd 05-Building_Multi-Container_Applications_with_Docker\frontend
 docker build -t goal-react:latest .
 # Run container frontend
-docker run -p 3000:3000 -it --network goal-network --name goal-frontend -v app/node_modules -v "C:\Users\Tuan\Desktop\my-docker-kubernates-learning\05-Building_Multi-Container_Applications_with_Docker\frontend:/app" goal-react:latest
+docker run -p 3000:3000 -it --name goal-frontend -v app/node_modules -v "C:\Users\Tuan\Desktop\my-docker-kubernates-learning\05-Building_Multi-Container_Applications_with_Docker\frontend:/app" goal-react:latest
 docker logs goal-frontend
